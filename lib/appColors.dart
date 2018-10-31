3c953efa8237b4519e6c3051b61172c5e281d6b5
“earyzhe” <earyzhe@gmail.com>
2018-10-31 09:17:59 +0000

    Initial commit

---
+++ lib/appColors.dart
@@ -0,0 +1,39 @@
+// import 'package:flutter/material.dart';
+
+// class AppColors {
+
+//   static Color getColor(ColorType font) {
+//     Color returnValue;
+
+//     switch (font) {
+//       case ColorType.background:
+//         returnValue = Color.fromARGB(250, 50, 50, 50);
+//         break;
+//         case ColorType.tint:
+//         returnValue = Color.fromARGB(250, 208 , 136, 75);
+//         break;
+
+//         case ColorType.white:
+//         returnValue = Color.fromARGB(250, 230, 230, 230);
+//         break;
+
+//         case ColorType.toolBar:
+//         returnValue = Color.fromARGB(250, 25, 25, 25);
+//         break;
+
+//         case ColorType.popupBackground:
+//         returnValue = Color.fromARGB(50, 0, 0, 0);
+//         break;
+
+//         case ColorType.primarySwatch:
+//         returnValue = Color.fromARGB(50, 0, 0, 0);
+//         break;
+
+//       default:
+//         returnValue = Color.fromARGB(250, 230, 230, 230);
+//     }
+//     return returnValue;
+//   }
+// }
+
+// enum ColorType { background, tint, white , toolBar,  popupBackground, primarySwatch}
---
+++ lib/strings.dart
@@ -0,0 +1,245 @@
+
+
+class StringLabels {
+    
+        static String forgottonPassword = 'Forgotton your password?.';
+        static String welcomeToDialIn = 'Welcome to Dial In.';
+        static String logInWithDetails = 'Log in with your email and password.';
+        static String syncData = "Sync data.";
+        static String userBig = "User";
+        static String userName = "User name";
+        static String anyExtraInformation = "Any other informaton about the brew?";
+        static String anyExtraBaristaInformation = "Any other informaton about the barista?";
+        static String writeADescriptionOfMethod = "Write a description of your brew method.";
+        static String overview = "Overview";
+        static String newe = "New";
+        static String editing = "Editing";
+        static String saved = "Saved";
+        static String email = "Email";
+        static String password = "Password";
+        static String name = "Name";
+        static String user = "User";
+        static String enterValueGrams = "Enter value (g)";
+        static String brewWeight = "Brew weight (g)";
+        static String successfullyRetrievedForUpdating = "Successfully retrieved for updating";
+        static String details = "Details";
+        static String back = "Back";
+        static String save = "Save";
+        static String edit = "Edit";
+        static String syncing = "Syncing";
+        
+        static String notes = "Notes";
+        static String blankItem = "Blank item";
+        static String baristaLevel = "Barista level";
+        static String baristaName = "Barista name";
+        static String baristaNotes = "Barista notes";
+        
+        static String addBarista = "Add barista";
+        static String account = "Account";
+        static String editAccountInfo = "Edit account info";
+        static String shareSettings = "Share Settings";
+        static String brewSettings = "Brew settings";
+        static String weightUnit = "Weight unit";
+        static String temparatureUnit = "Temparature unit";
+        static String help = "Help";
+        static String feedback = "Feedback";
+        static String language = "Language";
+        static String about = "About";
+        static String website = "Website";
+        
+        
+        static String photoSource = "Photo source";
+        static String cancel = "Cancel";
+        static String photoLibrary = "Photo library";
+        static String camera = "Camera";
+        
+        static String chooseSource = "Choose a source";
+        
+        
+        static String altitude = "Altitude (masl)";
+        
+        static String method = "Method";
+        
+        static String dateTested = "Date tested";
+        static String selectCoffee = "Select coffee";
+        static String fillInScores = "Fill in scores";
+        static String maxScore = "20";
+        
+        static String beginner = "Beginner";
+        static String amatuer = "Amatuer";
+        static String junior  = "Junior";
+        static String intermediate = "Intermediate";
+        static String medium = "Medium";
+        static String professional  = "Professianal";
+        static String senior  = "Senior";
+        static String lead  = "Lead";
+        
+        
+        static String flat = "Flat";
+        static String conical = "Conical";
+        static String turkish  = "Turkish";
+        static String blade = "Blade";
+        
+        static String autoFill = "Autofill";
+        
+        
+        static String american = "American";
+        static String french = "French";
+        static String italian = "Italian";
+        static String cinnamon = "Cinnamon";
+        static String light = "light";
+        static String dark = "Dark";
+        static String mediumDark = "Medium - Dark";
+        static String newEngland = "New England";
+        static String city = "City";
+        static String fullCity = "Full City";
+        static String espresso = "Espresso";
+        static String filter = "Filter";
+        static String omni = "Omni";
+        static String vienna = "Vienna";
+        static String spanish = "Spanish";
+        
+        static String enterInfo = "Info?";
+        static String enterType = "Type?";
+        static String enterDescriptors = "Taste descriptors?";
+        static String enterScore = "Score? (1-5)";
+        static String giveDetails = "Details?";
+        static String enterValue = "Value?";
+        static String enterNicknameOptional = "Enter name?";
+        static String inputDate = "Date?";
+        
+        static String density = "Density mg/l";
+        static String moisture = "Moisture content %";
+        static String aW = "Water activity";
+        static String barista = "Barista";
+        static String level = "Level";
+        
+        static String originDetails = "Origin details";
+        
+        static String greenCoffeeDetails = "Green details";
+        static String roastedCoffeeDetails = "Roasting details";
+        
+        static String date = "Date";
+        
+        static String grindSetting = "Grind setting";
+        static String add = "Add";
+        static String settings = "Settings";
+        static String finalScore = "Final score";
+        static String enterRecipeDetails = "Enter your recipe details.";
+        static String tastingScoreTotal = "54";
+        static String body = "Body";
+        static String afterTaste = "AfterTaste";
+        static String descriptors = "Descriptors";
+        
+        static String enterDescription = "Enter description.";
+        
+        static String addCoffeeProfile = "Add a coffee";
+        static String addGrinderProfile = "Add a Grinder";
+        static String addWaterProfile = "Add water";
+        static String addBrewingEquipment =  "Add brewing equipment";
+        static String addNewRecipe = "Add new recipe";
+        
+        static String coffeeBeans = "Coffee beans";
+        static String nickname = "Name";
+        static String recipe = "Recipe";
+        static String coffee = "Coffee";
+        static String grinder = "Grinder";
+        static String brewingDose = "Dose (g)";
+        static String yield = "Yield (g)";
+        static String time = "Time";
+        static String water = "Water";
+        static String temparature = "Temparature ℃";
+        static String tds = "TDS %";
+        static String score = "Score";
+        static String preinfusion =  "Pre-infusion";
+        static String coffeeId = "Coffee ID";
+        static String producer = "Producer";
+        static String lot = "Lot";
+        static String farm = "Farm";
+        static String region = "Region";
+        static String country = "Country";
+        static String beanType =  "Bean Type";
+        static String beanSize = "Bean Size";
+        static String roastProfile = "Roast Profile";
+        static String roastDate = "Roast Date";
+        static String roasteryName = "Roastery name";
+        
+        
+        static String grinderId = "Grinder ID";
+        static String grinderMake = "Grinder Make";
+        static String grinderModel = "Grinder Model";
+        
+        static String make = "Make";
+        static String model  = "Model";
+        static String burrs = "Burrs";
+        static String testTemparature = "Test temparature ℃";
+        static String gh = "GH ppm";
+        static String kh = "KH ppm";
+        static String ppm = "Total ppm";
+        static String ph = "pH";
+        static String ghDisplay = "ppm GH";
+        static String khDisplay = "ppm KH";
+        static String ppmDisplay = "ppm Total";
+        static String brewingEquipment = "Equipment";
+        static String equipmentId = "Equipment ID";
+        
+        static String equipmentType = "Equipment Type";
+        static String equipmentMake = "Equipment Make";
+        static String equipmentModel = "Equipment Model";
+        
+        
+        static String type = "Type";
+        static String waterID = "Water ID";
+        static String celsius = "℃";
+        static String faranheit = "℉";
+        
+        static String logIn = "Log in";
+        static String createAccount = "Create Account";
+        
+        static String grams = "g";
+        static String percent = "%";
+        static String seconds = "secs";
+        static String minutes = "mins";
+        
+        
+        static String enterPrecentage = "Enter percentage";
+        static String enterWeightInGrams = "Enter weight in grams";
+        static String enterTemparature = "Enter temparature ℃";
+        static String wait = "Wait!";
+        static String enterNickname = "Name?";
+        
+        static String couldNotSignYouUp = "Could not sign you up";
+        static String signYouUp = "All signed up";
+        
+        static String nowLogIn = "You can now log in!";
+        static String deStringe = "DeStringe";
+        
+        static String tasteBalance = "Taste balance";
+        static String tactile = "Tactile";
+        static String flavour = "Flavour";
+        
+        static String taste = "Flavour";
+        static String sweet = "Sweet";
+        static String acidic = "Acidic";
+        static String balance = "Balance";
+        static String weight = "Weight";
+        static String texture = "Texture";
+        static String finish = "Finish";
+        static String bitter = "Bitter";
+        
+        
+        static String processingMethod = "Processing method";
+        static String washed = "Washed";
+        static String natural = "Natural";
+        static String semiWashed = "Semi-washed";
+        static String honey = "Honey";
+        static String whiteHoney = "White honey";
+        static String yellowHoney = "Yellow honey";
+        static String redHoney = "Red honey";
+        static String blackHoney = "Black honey";
+        static String pulpNatural = "PulpNatural";
+        static String wetHulled = "Wet hulled";
+}
+ 
+    
+  
