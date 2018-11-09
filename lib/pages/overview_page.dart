41ea40adda2f162a5e1d30c46c1aff93b34d2891
“earyzhe” <earyzhe@gmail.com>
2018-11-09 11:25:18 +0000

    All working ok

--- lib/pages/overview_page.dart
+++ lib/pages/overview_page.dart
@@ -8,6 +8,17 @@ import '../appColors.dart';
 
 
 class OverviewPage extends StatelessWidget{
+
+TabController _tabBarController;
+nt _bottomNavbarIndex;
+
+@override
+void initState(){
+
+// _tabBarController = new TabController( length: 1, vsync: this)
+
+}
+
 //
 /// UI Build
 ///
@@ -20,8 +31,8 @@ class OverviewPage extends StatelessWidget{
       ///
       appBar: AppBar( backgroundColor: Colors.orange.withOpacity(0.8),title: Text(StringLabels.overview, style: TextStyle( fontWeight: FontWeight.w700),), automaticallyImplyLeading: false,
       leading: RawMaterialButton( onPressed: () => Navigator.pop(context), 
-            child: Container(padding: EdgeInsets.all(20.0) ,decoration: BoxDecoration(image: DecorationImage( image: AssetImage('assets/images/logout.png'), fit: BoxFit.cover)),),), ),
-      
+            child: Text('Log out'),), ),
+      // Container(padding: EdgeInsets.all(20.0) ,decoration: BoxDecoration(image: DecorationImage( image: AssetImage('assets/images/logout.png'), fit: BoxFit.cover)),)
       ///Other buttons
       //  actions: <Widget>[
       //     FlatButton(onPressed:() { Navigator.pop(context);}, child:  Text(StringLabels.logOut, style: TextStyle(color: Colors.black, fontSize: 15.0),),)
@@ -61,22 +72,8 @@ class OverviewPage extends StatelessWidget{
                 SegmentControlButton('Social'),
                 Flex(direction: Axis.horizontal )
 
-
-                // Container(width: 20.0 ,height: 20.0 ,child:RaisedButton(child:  Column(children: <Widget>[Image.asset('assets/images/user.png'),Text('Social')],),),),
-                // Container(width: 20.0 ,height: 20.0 ,child:RaisedButton(child:  Column(children: <Widget>[Image.asset('assets/images/whiteRecipe200X200.png'),Text('Recipe')],),),),
-                // Container(width: 20.0 ,height: 20.0 ,child:RaisedButton(child:  Column(children: <Widget>[Image.asset('assets/images/coffee-beanSmaller512x512.png'),Text('Coffee')],),),),
-                // Container(width: 20.0 ,height: 20.0 ,child:RaisedButton(child:  Column(children: <Widget>[Image.asset('assets/images/drop.png'),Text('Water')],),),)
               ],),),
-              // CupertinoSegmentedControl( onValueChanged: (dynamic) => print("in here"),
-              // children: const <int, Widget>{
 
-              //     0: SegmentControlButton(StringLabels.recipe,Image.asset('assets/images/drop.png'))
-              //     // 0: Text('Social'),
-              //     // 1: Text('Recipe'),
-              //     // 2: Text('Coffee'),
-              //     // 3: Text('Water'),     
-                
-              //   }),
               ProfileCard()
  
           ],          
@@ -86,17 +83,36 @@ class OverviewPage extends StatelessWidget{
       ),
        ]
      ),
-
-      //  bottomNavigationBar: Material(color:Colors.black , child: BottomNavigationBar(  fixedColor: Colors.black,  iconSize: 10.0,  type: BottomNavigationBarType.shifting,
-      //    items: <BottomNavigationBarItem>[
-          //  BottomNavigationBarItem( backgroundColor: Colors.transparent, title: Text('Add'), icon: Image.asset('assets/images/addProfileButtonWithOrangeTint.png')),
-          //  BottomNavigationBarItem( backgroundColor: Colors.transparent, title: Text('Add'), icon: Image.asset('assets/images/addProfileButtonWithOrangeTint.png')),
-          //  BottomNavigationBarItem( backgroundColor: Colors.transparent, title: Text('Add'), icon: Image.asset('assets/images/addProfileButtonWithOrangeTint.png'))       ]
-      //  ))
+     
      floatingActionButton: FloatingActionButton(onPressed: ()=> {}, child: Icon(Icons.add),),
+     bottomNavigationBar: BottomNavigationBar(
+      // currentIndex: _bottomNavbarIndex, 
+      items: [
+
+        BottomNavigationBarItem(icon: Icon(Icons.add), title: Text("Twat")),
+        BottomNavigationBarItem(icon: Icon(Icons.add), title: Text("Twat")),
+        BottomNavigationBarItem(icon: Icon(Icons.add), title: Text("Twat"))
+
+      ],)
+    
       );
     }
 }
+
+
+///
+/// Tab bar controller
+/// 
+
+
+
+
+
+
+
+
+
+
 
 
 ///
@@ -121,8 +137,13 @@ class SegmentControlButton extends StatelessWidget{
 
     Container(width: 50.0 ,height: 50.0 ,child:
     RawMaterialButton(onPressed: () =>{}, child:  
-    Column(children: <Widget>[Text(text, style: TextStyle( fontSize: 10.0 ))],),),);
+    Column(children: <Widget>[
 
+      Icon(Icons.add),
+      Text(text, style: TextStyle( fontSize: 10.0 ))],),),
+      
+      );
+      
 
               // Card( 
               //   child: Column( 
@@ -187,7 +208,50 @@ class ProfileCard extends StatelessWidget{
 }
 }
 
+///Profile card
+class SocialProfle extends StatelessWidget{
+@override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row( 
+        children: <Widget>[
+          
+          ///
+          /// Profile picture
+          /// 
+          Container( child: CircularPicture(Image.asset('assets/images/user.png'), 60.0)),
 
+          Expanded(child:
+          Row( children: <Widget>[
+
+            ///
+            /// Main name and secondnary details
+            /// 
+            Expanded(child:Container( padding: EdgeInsets.all(0.0), child:
+            Column(crossAxisAlignment: CrossAxisAlignment.start,children: <Widget>[
+              Container(margin: EdgeInsets.all(10.0),child: Text('Main')),
+              Container(margin: EdgeInsets.all(10.0), child: Text('Second'),)
+              ]
+              ) 
+            )),
+            ///
+            /// Third and fourth details
+            ///
+            Expanded(child:Container( padding: EdgeInsets.all(0.0), child:
+            Column(crossAxisAlignment: CrossAxisAlignment.end,children: <Widget>[
+              Container(margin: EdgeInsets.all(10.0),child: Text('Third')),
+              Container(margin: EdgeInsets.all(10.0), child: Text('Fourth'),)
+              ]
+              ) 
+            ))
+        ]
+      )
+      )
+        ]
+      )
+    );
+}
+}
 
 class UserCard extends StatelessWidget{
 @override
